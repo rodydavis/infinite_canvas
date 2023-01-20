4943e7f2bf8aa5718e0485009ce7765c57623979
rodydavis <rodydavis@users.noreply.github.com>
2023-01-20 00:03:00 +0000

    deploy: 9c7c89d5160e7ee8d571d995f3d610cf73c95cf7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
-"index.html": "d0197a805b4cad5fae47c69a6a41ea81",
-"/": "d0197a805b4cad5fae47c69a6a41ea81",
-"main.dart.js": "632a7663787917a4b31e0f1900972790",
+"index.html": "ccfd0efe1ad26e225e42d4245c49610e",
+"/": "ccfd0efe1ad26e225e42d4245c49610e",
+"main.dart.js": "f9eab34918c4e79b323265b25b1ad7ff",
 "manifest.json": "0867c3e13649ac4d06fe34b7b3ddce08",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
